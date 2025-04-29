e15b87bfe7168555b757bcd384736d3335dc9683
BriceLucifer <2376671337@qq.com>
2025-04-30 02:42:33 +0800

    add GroupsyncWrite

---
+++ src/group_sync_write.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub struct GroupSyncWrite {
+    // Define fields here
+}
--- src/scsci.rs
+++ src/scsci.rs
@@ -1,4 +1,4 @@
-use crate::protocol_packet_handler::ProtocolPacketHandler;
+use crate::{group_sync_write::GroupSyncWrite, protocol_packet_handler::ProtocolPacketHandler};
 
 #[derive(Debug)]
 pub struct Scscl {
