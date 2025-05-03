f151a7ac76e5f0e630442ee54c654d8e4890ce8c
BriceLucifer <2376671337@qq.com>
2025-05-03 14:36:26 +0800

    update

--- src/scsci.rs
+++ src/scsci.rs
@@ -1,7 +1,11 @@
 use crate::{group_sync_write::GroupSyncWrite, protocol_packet_handler::ProtocolPacketHandler};
 
+/*
+    python use inheritance with PortHandler
+    I use group_sync_write for the whole thing
+*/
+
 #[derive(Debug)]
 pub struct Scsci {
-    protocol_packet_handler: ProtocolPacketHandler,
     group_sync_write: GroupSyncWrite,
 }
